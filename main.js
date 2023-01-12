ff362be5b6bee6f715247db08cd7e7aabe9decce
Veronica Orozco <93060985+VLOrozco@users.noreply.github.com>
2023-01-12 00:32:03 -0600

    Nested submit function and added keypress function for

--- main.js
+++ main.js
@@ -6,10 +6,19 @@ const submit = document.getElementById('submit');
 const messageDisplay = document.getElementById('messageDisplay');
 
 // add event listener to use the function passMessage when the submit button is selected
-submit.addEventListener('click', passMessage);
-
-function passMessage() {
+submit.addEventListener('click', function passMessage() {
   messageDisplay.innerHTML = message.value;
 
   document.getElementById("messageForm").reset();
-}
+});
+
+// Execute a function when the user presses the "Enter" key on the keyboard
+message.addEventListener('keypress', function(event){
+   // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter"){
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the submit button element with a click
+    submit.click();
+  }
+});
